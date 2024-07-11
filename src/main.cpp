068214d1a4ff5f13a41f71c4dbd2b7d06f7effa8
seru01 <reynaldbartiana2023@gmail.com>
2024-07-11 11:44:50 +0800

    Correcting Time

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
  * @authors Reynald Bartiana
  * @version 0.1.0
  * @date Created On 07/11/2024
- * @date Last Modified On 017/11/2024
+ * @date Last Modified On 07/11/2024
  * @copyright Copyright (c) 2024
  *************************************/
 #include <Arduino.h>
