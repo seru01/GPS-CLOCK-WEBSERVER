a5841ccf71fdb6eadd99af8774b5cd909963eb35
seru01 <reynaldbartiana2023@gmail.com>
2024-07-11 17:17:20 +0800

    test1

--- src/main.cpp
+++ src/main.cpp
@@ -7,15 +7,64 @@
  * @copyright Copyright (c) 2024
  *************************************/
 #include <Arduino.h>
+#include <WiFi.h>
+#include <SPIFFS.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <TinyGPS++.h>
+#include <HardwareSerial.h>
 
+IPAddress IP(10, 0, 0, 1);
+IPAddress gateway(10, 0, 0, 1);
+IPAddress subnet(255, 255, 255, 0);
+AsyncWebServer server(80);
 
+static const int RXPin = 16, TXPin = 17;
+static const uint32_t GPSBaud = 9600;
 
-//SETUP
+// The TinyGPS++ object
+TinyGPSPlus gps;
+
+// The serial connection to the GPS device
+HardwareSerial GPS(1);
+
+// SETUP
 void setup() {
+    GPS.begin(GPSBaud, SERIAL_8N1, RXPin, TXPin); 
+    Serial.begin(115200);
+    
+    WiFi.mode(WIFI_AP);
+    String ssid = "GPSCLOCK:" + WiFi.macAddress(); 
+    const char* password = "1234567890";
+
+    WiFi.softAPConfig(IP, gateway, subnet);
+    WiFi.softAP(ssid.c_str(), password);
+
+    // Initialize SPIFFS
+    if (!SPIFFS.begin()) {
+        Serial.println("Failed to mount SPIFFS");
+        return;
+    }
+    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+         request->send(SPIFFS, "/index.html", "text/html");
+    });
+
+    // BEGIN SERVER
+    server.begin();
+    Serial.println("Web Server Begins ...");
 }
 
-
-//LOOP
+// LOOP
 void loop() {
+    while (GPS.available()) {
+        gps.encode(GPS.read());
 
+        if (gps.time.isUpdated()) {
+            // Extract time in hh:mm:ss format
+            char timeBuffer[10];
+            sprintf(timeBuffer, "%02d:%02d:%02d", gps.time.hour(), gps.time.minute(), gps.time.second());
+            Serial.print("GPS TIME: ");
+            Serial.println(timeBuffer);
+        }
+    }
 }
